cf77a9b40b9960a1afbe166fb43333d144fcc148
jittrawanpadsang <s6812022611201@email.kmutnb.ac.th>
2025-08-08 23:39:45 +0700

    Add files via upload

---
+++ coding_w06_01.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+ 
+int main(){ 
+    int a=5;//เก็บค่าตัวแปร 
+    int b=17;//เก็บค่าตัวแปร 
+    float c=8.5;//เก็บค่าตัวแปร 
+    float d=4.0;//เก็บค่าตัวแปร 
+ 
+    printf("assign >> a=%db=%dc=%.1fd=%.1f\n",a,b,c,d);//แสดงคค่าตัวแปรทั้งหมด 
+    printf("d+a=%.1f\n",d+a);//แสดงผลลัพธ์ d+a 
+    printf("a-b=%d\n",a-b);//แสดงผลลัพธ์ a-b 
+    printf("c*d=%.1f\n",c*d);//แสดงผลลัพธ์ c*d 
+    printf("a*c=%.1f\n",a*c);//แสดงผลลัพธ์ a*c 
+    printf("c/d=%.3f\n",c/d);//แสดงผลลัพธ์ c/d 
+    printf("b/c=%.1f\n",b/c);//แสดงผลลัพธ์ b/c 
+    printf("a%%b=%d\n",a%b);//แสดงผลลัพธ์ a%b 
+    printf("c%%a=%d\n",((int)c)%a);//แสดงผลลัพธ์ c % a โดยเปลี่ยน c เป็น int ก่อน  
+    printf("c%%d=%f\n",((int)c)%((int)d));//แสดงผลลัพธ์ c % d เปลี่ยน เป็น int 
+     
+    return 0; 
+} 
